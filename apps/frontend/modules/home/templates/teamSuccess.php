bdd273877a52580adae3060df3a974da7cff90ab
Matthew Skomarovsky <matthew@verypolite.com>
2012-12-12 22:23:28 -0500

    updated team page

--- apps/frontend/modules/home/templates/teamSuccess.php
+++ apps/frontend/modules/home/templates/teamSuccess.php
@@ -8,6 +8,4 @@
 
 <p>LittleSis is a project of <?php echo link_to('Public Accountability Initiative', 'http://public-accountability.org') ?>, a 501(c)3 organization focused on corporate and government accountability. Our mission is to facilitate and produce investigative research that promotes transparent and accountable governance. PAI was founded in early 2008 by a group of activists, public interest lawyers, and academics associated with leading universities and major social change organizations.</p>
 
-<p><img src="http://public-accountability.org/wp-content/uploads/pailogo.jpg" /></p>
-
 </span>
