382c5166d00efc8fb0cc27c13f4e320c507537a5
Matthew Skomarovsky <matthew@verypolite.com>
2015-05-14 10:55:51 -0400

    fixed bugs in sample sphinx config

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -1,7 +1,7 @@
 <?php
 // this check prevents access to debug front conrollers that are deployed by accident to production servers.
 // feel free to remove this, extend it or make something more sophisticated.
-if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
+if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')) && $_SERVER['HTTP_HOST'] != 'ls.dev')
 {
   die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
