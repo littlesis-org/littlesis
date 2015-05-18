fd47373d3ab63ddd5af946fc0e6f8ef7760111c2
Matthew Skomarovsky <matthew@verypolite.com>
2015-05-18 15:41:43 -0400

    added setup script

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+cp config/ProjectConfiguration.class.php.dist config/ProjectConfiguration.class.php
+cp config/databases.yml.dist config/databases.yml
+cp web/.htaccess.dist web/.htaccess
+cp apps/frontend/config/app.yml.dist apps/frontend/config/app.yml
+cp apps/frontend/config/settings.yml.dist apps/frontend/config/settings.yml
+cp apps/api/config/app.yml.dist apps/api/config/app.yml
+cp apps/api/config/settings.yml.dist apps/api/config/settings.yml
+cp config/sphinx.conf.dist config/sphinx.conf
+./symfony fix-perms
+mkdir -p data/sfWebBrowserPlugin/sfCurlAdapter
+touch data/sfWebBrowserPlugin/sfCurlAdapter/cookies.txt
+mkdir tmp
+chmod -R 777 tmp
+mkdir log/sphinx
+touch log/sphinx/searchd.log
+touch log/sphinx/query.log
+touch log/sphinx/searchd.pid
+chmod a+w log/sphinx/*
