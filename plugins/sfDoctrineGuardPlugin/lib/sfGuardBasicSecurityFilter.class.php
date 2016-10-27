630795cf521475032c09d0b01609d440fdbfd382
ziggy <ziggy@elephant-bird.net>
2016-10-27 19:35:26 -0400

    Check for rails session cookie with security filter.
    This avoids a redirect by checking the rails session cookie even if there
    is no symfony cookie.

--- plugins/sfDoctrineGuardPlugin/lib/sfGuardBasicSecurityFilter.class.php
+++ plugins/sfDoctrineGuardPlugin/lib/sfGuardBasicSecurityFilter.class.php
@@ -19,6 +19,33 @@ class sfGuardBasicSecurityFilter extends sfBasicSecurityFilter
 {
   public function execute ($filterChain)
   {
+      $context = $this->getContext();
+      $user = $context->getUser();
+      $firstCall = $this->isFirstCall();
+
+      if ($firstCall && (!$user->isAuthenticated() || !$user->getGuardUser())) 
+      {
+          if ($cookie = $context->getRequest()->getCookie('_lilsis_session')) 
+          {
+              $sql = "SELECT data from sessions WHERE session_id = ?";
+              $db = Doctrine_Manager::connection();
+              $stmt = $db->execute($sql, array($cookie));
+              $results = $stmt->fetchAll();
+            
+              if (count($results) > 0) {
+                  $sf_user_id = json_decode($results[0]["data"])->value->sf_user_id;
+                  $q = Doctrine_Query::create()
+                     ->from('sfGuardUser')
+                     ->where('id = ?', $sf_user_id);
+
+                  $sf_user = $q->fetchOne();
+                  if ($sf_user)
+                  {
+                      $user->signIn($sf_user);
+                  }
+              }
+          }
+      }
 
     // if ($this->isFirstCall() and !$this->getContext()->getUser()->isAuthenticated())
     // {
